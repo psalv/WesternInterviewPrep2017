0b2e9df5b449ace781be9d03883912b58d2ff428
psalv <psalvato@uwo.ca>
2017-09-23 11:16:51 -0400

    Finished #670

---
+++ Wk1/670_MaximumSwap.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def maximumSwap(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        numArray = []
+        for i in str(num):
+            numArray.append(int(i))
+
+        for currentDigit in range(len(numArray)):
+            largest = -1
+            largestIndex = -1
+            for examineDigit in range(currentDigit + 1, len(numArray)):
+                if numArray[currentDigit] < numArray[examineDigit] and largest < numArray[examineDigit]:
+                    largest = numArray[examineDigit]
+                    largestIndex = examineDigit
+            if largest != -1:
+                temp = numArray[currentDigit]
+                numArray[currentDigit] = largest
+                numArray[largestIndex] = temp
+
+                buildNum = ''
+                for i in numArray:
+                    buildNum += str(i)
+                return int(buildNum)
+
+        return num
+
+
+print(Solution.maximumSwap(None, 129))
+print(Solution.maximumSwap(None, 193))
+print(Solution.maximumSwap(None, 321))
+
+
