ac9b568026d2a343c33f80a8b20a6b676183370f
psalv <psalvato@uwo.ca>
2017-12-19 21:04:06 -0500

    problems

--- Coaching/problems.py
+++ Coaching/problems.py
@@ -45,68 +45,45 @@ so don't try to start with an ooptimal/clever solution, this can come afterwards
 
 
 
----
+Strategy:
+
+    Clarify problem
+
+    Define approach
+
+    Propose solution
+        Think about whats deliverable: do I need a class?
+        Verbally propose the solution before coding, so you can find mistakes faster
+
+    Consider info or constraints
+
+    Propose alternative solution // probably wont be able to do this fully but note the other and pros/cons
+
+    Implement solution
+        Pay attention to corner cases (Identify them and say when you're dealing with them)
+        Your code will reflect your abilities greatly since it goes to the hiring committee
+
+    Refine solution
+        Asks questions before going further
 
 
-Clarify problem
-Define approach
-Priopose solutiojn
-Consider info or constraints
-Propose alternative solution // probably wont be able to do this fully but note the other and pros/cons
-Implment solution
+Feel free to say what you plan on refining as you code.
 
-    pay attention to corner cases --> identify them and tell the interviewer when you're handling them
+Don't forget to state how you would test the code and run through a simple example, statign potential problems.
 
-    your code will reflect your abilities greatly
-        hriign panel will see your code
-        
+Talk about the time/space tradeoff, and state the choices that you are making.
     
-learn hwo to do mod for negative numbers
+Refine later !! Talk through the naive solution, consider big/small input and improvements.
+    
+It isn't a memory test, don't be afraid to ask API related questions, if you don't remember a call make one up (but say so).
 
-whats deliverable: do i need a class?
-
-
-** verbally propose solution before coding (faster to find mistakes)
-ask if the interviewer has questions before refining
-feel free to say that you will refine
-
-
-want to shw that we've thought about testing the code
-    think about potential problems
-    
-    
-    
-    
-    
-    
-    
-tiem/space tradeoff
-    choose sol'n you can omplement in interview time frame
-    state choices to interviewer
-    
-optimizing
-    refine later!!
-    tak through naive soln and impprove
-    consider various scenarios: input is big or small?
-    
-API
-    this is not a memory test, if you cant remember and API ask!!!      // make up the API call if you don't know and say that you are
-
-testing:
-    say what cases you would test
-    eserve time to work on a small case and edge cases
 """
 
-# imagine large bucket of coloured marbles, algorithm to pull out random
 
-# pass in as data structure
+# Question: develop an algorithm to pick a coloured marble at random from a bucket full of marbles.
 
-# with or without replacement
+# Need to decide on data structure, with/without replacement
 
-
-# array each index is a colour with counter of number
-# then predict chance
-# pay attention to off by 1's
 import random
 def selectMarbles(marbles):
     n = 0
